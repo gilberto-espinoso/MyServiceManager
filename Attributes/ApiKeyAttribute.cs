bdbd0c5940369820258225e723bbea26b5c60419
Gilberto Espinoso <gilbertosilvaespinosa@gmail.com>
2023-05-22 22:11:10 -0300

    Refactor - new keys

--- Attributes/ApiKeyAttribute.cs
+++ Attributes/ApiKeyAttribute.cs
@@ -1,38 +1,35 @@
-using System;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
-namespace MyServiceManager.Attributes
-{
-    [AttributeUsage(validOn: AttributeTargets.Class | AttributeTargets.Method)]
-    public class ApiKeyAttribute : Attribute, IAsyncActionFilter
-    {
-        public async Task OnActionExecutionAsync(
-            ActionExecutingContext context, 
-            ActionExecutionDelegate next)
-        {
-            if (!context.HttpContext.Request.Query.TryGetValue(Configuration.ApiKeyName, out var extractedApiKey))
-            {
-                context.Result = new ContentResult()
-                {
-                    StatusCode = 401,
-                    Content = "ApiKey não encontrada"
-                };
-                return;
-            }
-        
-            if (!Configuration.ApiKey.Equals(extractedApiKey))
-            {
-                context.Result = new ContentResult()
-                {
-                    StatusCode = 403,
-                    Content = "Acesso não autorizado"
-                };
-                return;
-            }
+namespace MyServiceManager.Attributes;
 
-            await next();
+[AttributeUsage(validOn: AttributeTargets.Class | AttributeTargets.Method)]
+public class ApiKeyAttribute : Attribute, IAsyncActionFilter
+{
+    public async Task OnActionExecutionAsync(
+        ActionExecutingContext context, 
+        ActionExecutionDelegate next)
+    {
+        if (!context.HttpContext.Request.Query.TryGetValue(Configuration.ApiKeyName, out var extractedApiKey))
+        {
+            context.Result = new ContentResult()
+            {
+                StatusCode = 401,
+                Content = "ApiKey não encontrada"
+            };
+            return;
         }
+        
+        if (!Configuration.ApiKey.Equals(extractedApiKey))
+        {
+            context.Result = new ContentResult()
+            {
+                StatusCode = 403,
+                Content = "Acesso não autorizado"
+            };
+            return;
+        }
+
+        await next();
     }
 }
